39b8d1158009825b742646f223139e9461d431cf
Marcus Hoffmann <bubuiic@aol.com>
2015-03-17 16:24:28 +0100

    Initial commit

---
+++ desktop/src/com/basketball/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.basketball.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.basketball.Bastketball;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Bastketball(), config);
+	}
+}
